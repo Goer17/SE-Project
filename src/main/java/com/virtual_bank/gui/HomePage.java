df152dc0d9ea12faf973e545338d429723cb90c6
lyy <2950664961@qq.com>
2024-05-08 01:23:01 +0800

    HomePage baseline

--- src/main/java/com/virtual_bank/gui/HomePage.java
+++ src/main/java/com/virtual_bank/gui/HomePage.java
@@ -5,9 +5,19 @@ import java.awt.BorderLayout;
 import javax.swing.*;
 
 public class HomePage extends JPanel {
+    private JPanel missionPanel;
+    private JPanel targetPanel;
+
     public HomePage(BaseFrame baseFrame) {
         if (baseFrame.sessionManager.isLoggedIn()) {
-            this.add(new JLabel("<html><h2>Hello, " + baseFrame.sessionManager.getUsername() + ".👋</h2></html>."), BorderLayout.CENTER);
+            this.setLayout(new BorderLayout());
+            this.add(new JLabel("<html><h2>Hello, " + baseFrame.sessionManager.getUsername() + ".👋</h2></html>."), BorderLayout.NORTH);
+            this.missionPanel = new JPanel();
+            this.missionPanel.add(new JLabel("Missions panel"));
+            this.targetPanel = new JPanel();
+            this.targetPanel.add(new JLabel("Targets panel"));
+            this.add(this.missionPanel, BorderLayout.EAST);
+            this.add(this.targetPanel, BorderLayout.WEST);
         }
         else {
             this.add(new JLabel("<html><h2>Please login first :)</h2></html>"), BorderLayout.CENTER);
