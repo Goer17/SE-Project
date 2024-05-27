d65758225d34d4489cc9e16569b797e32fe047f8
JOEzzzzzzzzz <103730644+JOEzzzzzzzzz@users.noreply.github.com>
2024-05-27 21:02:52 +0800

    Update annotation

--- src/main/java/com/virtual_bank/core/Mission.java
+++ src/main/java/com/virtual_bank/core/Mission.java
@@ -3,46 +3,46 @@ package com.virtual_bank.core;
 import java.util.UUID;
 
 public class Mission {
-    private String mid; // 任务ID
-    private String content; // 内容
-    private int reward; // 奖励
+    private String mid; // Task ID
+    private String content; 
+    private int reward; 
 
-    public String getMid() { // 获取任务ID
+    public String getMid() { 
         return mid;
     }
 
-    public void setMid(String mid) { // 设置任务ID
+    public void setMid(String mid) { 
         this.mid = mid;
     }
 
-    public String getContent() { // 获取内容
+    public String getContent() { 
         return content;
     }
 
-    public void setContent(String content) { // 设置内容
+    public void setContent(String content) { 
         this.content = content;
     }
 
-    public int getReward() { // 获取奖励
+    public int getReward() {
         return reward;
     }
 
-    public void setReward(int reward) { // 设置奖励
+    public void setReward(int reward) { 
         this.reward = reward;
     }
 
-    public Mission(String mid, String content, int reward) { // 构造函数，传入任务ID、内容和奖励
-        if ("#new".equals(mid)) { // 如果任务ID为"#new"，则生成新的唯一任务ID
+    public Mission(String mid, String content, int reward) { // Constructor, passing in the task ID, content, and reward
+        if ("#new".equals(mid)) { // If the task ID is "#new", a new unique task ID is generated
             this.setMid(UUID.randomUUID().toString());
         }
         else {
-            this.setMid(mid); // 否则使用传入的任务ID
+            this.setMid(mid); // Otherwise, the passed task ID is used
         }
-        this.setContent(content); // 设置内容
-        this.setReward(reward); // 设置奖励
+        this.setContent(content); // Set content
+        this.setReward(reward); // Set reward
     }
 
-    public String description() { // 描述方法，返回任务描述字符串
+    public String description() { // Description method, which returns a task description string
         return this.content + " (reward:" + this.reward + ")";
     }
 }
