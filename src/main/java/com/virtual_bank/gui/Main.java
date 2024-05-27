dc3dc459a4b3300a12f559ee38ed0b3e58b20ee2
Linhan Song <146309234+Kitagawayyds@users.noreply.github.com>
2024-05-27 21:13:31 +0800

    添加注释

--- src/main/java/com/virtual_bank/gui/Main.java
+++ src/main/java/com/virtual_bank/gui/Main.java
@@ -6,9 +6,9 @@ import com.virtual_bank.core.SessionManager;
 
 public class Main {
     public static void main(String[] args) {
-        SessionManager sessionManager = SessionManager.getInstance();
+        SessionManager sessionManager = SessionManager.getInstance(); // 获取 SessionManager 的单例实例
 
-        SwingUtilities.invokeLater(() -> {
+        SwingUtilities.invokeLater(() -> { // 运行GUI 
             new BaseFrame(sessionManager).setVisible(true);
         });
     }
