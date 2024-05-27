b733410bb52e0aaa4b63a8df326277e4ce4d6a9b
JOEzzzzzzzzz <103730644+JOEzzzzzzzzz@users.noreply.github.com>
2024-05-27 21:24:30 +0800

    Update annotation

--- src/main/java/com/virtual_bank/gui/ProfilePage.java
+++ src/main/java/com/virtual_bank/gui/ProfilePage.java
@@ -17,11 +17,8 @@ public class ProfilePage extends JPanel {
     private Button depositButton;
     private Button withdrawButton;
 
-    // 用户个人资料页面
     public ProfilePage(BaseFrame baseFrame) {
-        // 检查用户是否登录
         if (baseFrame.sessionManager.isLoggedIn()) {
-            // 边距和布局
             this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
             this.setLayout(new BorderLayout());
 
@@ -30,21 +27,19 @@ public class ProfilePage extends JPanel {
             int money = XMLDBManager.findUser(username).getMoney();
             XMLDBManager.processMaturedDepositsForUser(currentUser.getUid());
 
-            // 余额面板
             balanceLabel = new JPanel();
             balanceLabel.add(new JLabel(getBalanceHtml(username, money)), BorderLayout.CENTER);
             this.add(balanceLabel, BorderLayout.WEST);
 
-            // 按钮
+
             Button depositButton = new Button("Deposit Money");
             Button withdrawButton = new Button("Withdraw Money");
             fixedDepositButton = new Button("Create Fixed Deposit");
 
-            // 按钮事件
+            // fixed deposit action listener
             fixedDepositButton.addActionListener(e -> {
                 FixedDepositDialog dialog = new FixedDepositDialog(baseFrame, currentUser);
 
-                // 定期存款后刷新
                 dialog.addFixedDepositListener(evt -> {
                     baseFrame.refresh();
                     baseFrame.switchToPanel("Profile");
@@ -56,54 +51,50 @@ public class ProfilePage extends JPanel {
             depositButton.addActionListener(e -> updateBalance(true, baseFrame));
             withdrawButton.addActionListener(e -> updateBalance(false, baseFrame));
 
-            // 添加到按钮面板
             JPanel buttonPanel = new JPanel();
             buttonPanel.add(depositButton);
             buttonPanel.add(withdrawButton);
             buttonPanel.add(fixedDepositButton);
             this.add(buttonPanel, BorderLayout.SOUTH);
 
-            // 交易记录
+            // transactions
             transactionsArea = new CuteTextArea(10, 30);
             transactionsArea.setEditable(false);
             updateTransactionsDisplay(currentUser.getUid());
             JScrollPane transactionsScrollPane = new JScrollPane(transactionsArea);
 
-            // 定期存款显示
+            // fixed Deposits
             fixedDepositsArea = new CuteTextArea(10, 30);
             fixedDepositsArea.setEditable(false);
             updateFixedDepositsDisplay(currentUser.getUid());
             JScrollPane fixedDepositsScrollPane = new JScrollPane(fixedDepositsArea);
 
-            // 创建上两个面板
+
             JPanel displayPanel = new JPanel(new GridLayout(1, 2));
             displayPanel.add(transactionsScrollPane);
             displayPanel.add(fixedDepositsScrollPane);
             this.add(displayPanel, BorderLayout.CENTER);
 
-            // 重绘
             this.revalidate();
             this.repaint();
         } else {
-            // 未登录时显示
             this.add(new JLabel("<html><h2>Please login first :)</h2></html>"), BorderLayout.CENTER);
         }
     }
 
-    // 显示用户余额
     private String getBalanceHtml(String username, int money) {
         return "<html><h3>Profile</h3><br><h3>" + username + "</h3><br>" +
                "<h3>Your account balance: " + "<span style='color: blue;'>" +
                money + "</span></h3></html>";
     }
 
-    // 更新余额
+   
     private void updateBalance(boolean isDeposit, BaseFrame baseFrame) {
         String transactionType = isDeposit ? "Deposit" : "Withdraw";
         CustomInputDialog dialog = new CustomInputDialog(null, transactionType, true);
         String input = dialog.showDialog();
-
-        // 取消输入则返回
+        
+        // Return if input is cancelled
         if (input == null) {
             return;
         }
@@ -118,7 +109,7 @@ public class ProfilePage extends JPanel {
             int currentMoney = currentUser.getMoney();
             int newBalance = isDeposit ? currentMoney + amount : currentMoney - amount;
 
-            // 余额检测
+            // Balance check
             if (!isDeposit && newBalance < 0) {
                 JOptionPane.showMessageDialog(this, "Insufficient funds!", "Error", JOptionPane.ERROR_MESSAGE);
                 return;
@@ -127,7 +118,7 @@ public class ProfilePage extends JPanel {
             currentUser.setMoney(newBalance);
             XMLDBManager.saveUser(currentUser);
 
-            // 创建新的交易记录
+            // Create a new transaction record
             Transaction newTransaction = new Transaction(currentUser.getUid(), transactionType, amount);
             XMLDBManager.addTransaction(newTransaction);
             
@@ -138,7 +129,7 @@ public class ProfilePage extends JPanel {
         }
     }
 
-    // 更新交易记录
+    // Update transaction records
     private void updateTransactionsDisplay(String uid) {
         List<Transaction> transactions = XMLDBManager.getTransactionsForUser(uid);
         transactionsArea.setText("");
@@ -147,7 +138,7 @@ public class ProfilePage extends JPanel {
         }
     }
 
-    // 更新定期存款
+    // Update fixed deposits
     private void updateFixedDepositsDisplay(String uid) {
         List<FixedDeposit> fixedDeposits = XMLDBManager.getFixedDepositsForUser(uid);
         fixedDepositsArea.setText("");
