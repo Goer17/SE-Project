7c4528e168cbc426006f1adf671b9f34fe911644
JOEzzzzzzzzz <103730644+JOEzzzzzzzzz@users.noreply.github.com>
2024-05-27 21:25:05 +0800

    Update annotation

--- src/main/java/com/virtual_bank/gui/Main.java
+++ src/main/java/com/virtual_bank/gui/Main.java
@@ -6,9 +6,9 @@ import com.virtual_bank.core.SessionManager;
 
 public class Main {
     public static void main(String[] args) {
-        SessionManager sessionManager = SessionManager.getInstance(); // 获取 SessionManager 的单例实例
+        SessionManager sessionManager = SessionManager.getInstance(); 
 
-        SwingUtilities.invokeLater(() -> { // 运行GUI 
+        SwingUtilities.invokeLater(() -> { 
             new BaseFrame(sessionManager).setVisible(true);
         });
     }
